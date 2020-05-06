fdbdf80c653d61b937c6e6eb51c4711cb3619179
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 22:29:21 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,5 @@
 module Findable
-  
-  module ClassFindableMethods
-    
-    def find_by_name(name)
-      all.detect {|obj| obj.name == name}
-    end
-    
+  def find_by_name(name)
+    all.detect {|obj| obj.name == name}
   end
-  
 end
