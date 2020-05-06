8c4a818d0af6eb433fa625a320eee204a5e9cd0a
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 21:06:13 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,19 +1,15 @@
 module Memorable
-  
-  module ClassMethods
-    def self.all
-      @@artists
-    end
 
-    def self.reset_all
-      self.all.clear
-    end
-  
-    def self.count
-      self.all.count
-    end
+  def self.all
+    @@artists
   end
-  
-  
-  
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
 end
