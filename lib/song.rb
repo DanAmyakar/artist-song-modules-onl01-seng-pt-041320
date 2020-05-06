dc8539738bf7efec9fc364dd025091352ff98298
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 22:16:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative './concerns/memorable'
+require_relative './concerns/findable'
 
 class Song
   
@@ -14,9 +15,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
