fd15b8632ce6914febe0abbaee97fb2de6e4ee70
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 21:27:12 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,15 @@
 module Memorable
 
-  def self.reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
 
-  def self.count
-    self.all.count
+    def self.count
+      self.all.count
+    end
   end
+  
+  
 
 end
